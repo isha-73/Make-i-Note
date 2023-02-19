026e476a7e3bb6cdace3438cbea6cff7a940fbd2
Isha Bule <ishabule753@gmail.com>
2023-02-19 18:11:32 +0530

    added note title

--- note.js
+++ note.js
@@ -4,6 +4,7 @@ let notesObj;
 showNotes();
 addbtn.addEventListener("click", function (e) {
     let addtxt = document.getElementById('addtxt');
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem('notes');
 
     if (notes == null) {
@@ -12,9 +13,14 @@ addbtn.addEventListener("click", function (e) {
         console.log(typeof (notes));
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addtxt.value);
+    let myObj = {
+        title: addTitle.value,
+        text: addtxt.value
+      }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addtxt.value = '';
+    addTitle.value = "";
     console.log(notesObj);
     showNotes();
 });
@@ -30,12 +36,11 @@ function showNotes() {
     notesObj.forEach(function (element, index) {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width:200px; text-align: center;">
-            <div class="cardBody">
-                <h5 class="card-title">Note${index + 1}</h5>
-                <p>${element}</p>
-                <input type="checkbox" id="imp${index}" name="IMPORTANT" />
-                <label for="imp${index}">Mark Important</label>
-                <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary" id="addBtn">Delete-Note</button>
+            <div class="cardBody" id="i${index}">
+                <h5 class="card-title">${index+1}.${element.title}</h5>
+                <p>${element.text}</p>
+               
+                <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary" >Delete-Note</button>
             </div>
         </div>`;
     });
@@ -74,16 +79,8 @@ searchtxt.addEventListener("input", function () {
         }
     })
 })
-let imp= document.getElementsByClassName('imp')
-imp.addEventListener('click',function(){
-    let note = document.getElementsByClassName('noteCard');
-    Array.from(note).forEach(function (element) {
-        let text = element.getElementsByTagName("input")[0].innerHTML;
-        if (text==true) {
-            element.style.backgroundcolour = "blue";
-        } else {
-            element.style.backgroundcolour = "none";
-        }
-    })
-    
-})
+
+// function impNote(index){
+//  let get = document.getElementById("index");
+//  get.style.backgroundColor="blue";
+// }
